cb13c39c1b1675b5697b9de4e1157827a011b881
Christopher Timm <timm@effective-webwork.de>
2019-02-26 14:23:42 +0100

    initial commit

---
+++ Configuration/TypoScript/Library/2000_page.ts
@@ -0,0 +1,53 @@
+
+# Default PAGE object:
+page = PAGE
+
+## Favicon
+#page.shortcutIcon = {$resDir}/Public/Icons/favicon.ico
+
+## CSS Dateien laden
+page.includeCSS {
+	main = {$resDir}/Public/Css/main.css
+}
+
+## JavaScript laden
+page.includeJSlibs {
+}
+
+page.includeJSFooterlibs {
+	jquery = {$resDir}/Public/JavaScript/vendor/jquery-1.11.2.min.js
+}
+
+page.includeJSFooter {
+	main = {$resDir}/Public/JavaScript/main.js
+}
+
+page.10 = FLUIDTEMPLATE
+page.10 {
+	partialRootPath = {$resDir}/Private/Partials
+	layoutRootPath = {$resDir}/Private/Layouts
+
+	variables {
+		contentMain < styles.content.get
+		contentAside < styles.content.get
+		contentAside.select.where = colPos = 1
+		contentAside.stdWrap {
+			wrap = <div>|</div>
+			required = 1
+		}
+	}
+
+	#file = {$resDir}/Private/Templates/DefaultTemplate.html
+}
+
+page.10.file.stdWrap.cObject = CASE
+page.10.file.stdWrap.cObject {
+	key.data = pagelayout
+
+	# Default Template
+	default = TEXT
+	default.value = {$resDir}/Private/Templates/DefaultTemplate.html
+
+	pagets__1 < .default
+
+}
---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Kitodo Presentation Package',
+	'description' => '',
+	'category' => 'distribution',
+	'author' => 'Christopher Timm',
+	'author_email' => 'timm@effective-webwork.de',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '7.6.2',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '7.6.2-7.9.99',
+			'fluid_styled_content' => '',
+		),
+		'conflicts' => array(
+			'css_styled_content' => '',
+		),
+		'suggests' => array(
+		),
+	),
+);
